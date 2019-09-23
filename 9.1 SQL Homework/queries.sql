0d134423202d9c219fcb6c19ce86e7e52f7349ee
wavyhair3 <diana.alderbashi@richmond.edu>
2019-09-22 20:37:01 -0400

    uncommeted create tables

--- 9.1 SQL Homework/queries.sql
+++ 9.1 SQL Homework/queries.sql
@@ -1,66 +1,66 @@
 
--- DROP TABLE IF EXISTS employees;
--- CREATE TABLE employees (
---     emp_no Integer NOT NULL,
---     birth_date Date NOT NULL,
---     first_name Varchar(30) NOT NULL,
---     last_name Varchar(30)  NOT NULL,
---     gender Varchar(1)  NOT NULL,
---     hire_date Date NOT NULL,
--- 	PRIMARY KEY (emp_no)
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
+    emp_no Integer NOT NULL,
+    birth_date Date NOT NULL,
+    first_name Varchar(30) NOT NULL,
+    last_name Varchar(30)  NOT NULL,
+    gender Varchar(1)  NOT NULL,
+    hire_date Date NOT NULL,
+	PRIMARY KEY (emp_no)
     
--- );
+);
 
 
--- DROP TABLE IF EXISTS departments;
--- CREATE TABLE departments (
---     dept_no Varchar(4) NOT NULL,
---     dept_name Varchar(30) NOT NULL,
---     PRIMARY KEY (dept_no)
--- );
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (
+    dept_no Varchar(4) NOT NULL,
+    dept_name Varchar(30) NOT NULL,
+    PRIMARY KEY (dept_no)
+);
 
 
--- DROP TABLE IF EXISTS titles;
--- CREATE TABLE titles (
---     emp_no Integer NOT NULL,
---     title Varchar(30) NOT NULL,
---     from_date Date NOT NULL,
---     to_date Date NOT NULL,
--- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
--- );
+DROP TABLE IF EXISTS titles;
+CREATE TABLE titles (
+    emp_no Integer NOT NULL,
+    title Varchar(30) NOT NULL,
+    from_date Date NOT NULL,
+    to_date Date NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
 
--- DROP TABLE IF EXISTS salaries;
--- CREATE TABLE salaries (
---     emp_no Integer NOT NULL,
---     salary Integer NOT NULL,
---     from_date Date NOT NULL,
---     to_date Date NOT NULL,
--- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
--- );
+DROP TABLE IF EXISTS salaries;
+CREATE TABLE salaries (
+    emp_no Integer NOT NULL,
+    salary Integer NOT NULL,
+    from_date Date NOT NULL,
+    to_date Date NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
 
 
 
--- DROP TABLE IF EXISTS dept_manager;
--- CREATE TABLE dept_manager (
---  dept_no Varchar(4) NOT NULL,
--- 	emp_no Integer NOT NULL,
--- 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
--- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
---  from_date Date NOT NULL,
---  to_date Date NOT NULL
+DROP TABLE IF EXISTS dept_manager;
+CREATE TABLE dept_manager (
+ dept_no Varchar(4) NOT NULL,
+	emp_no Integer NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+ from_date Date NOT NULL,
+ to_date Date NOT NULL
 	
--- );
+);
 
--- DROP TABLE IF EXISTS dept_emp;
--- CREATE TABLE dept_emp (
--- 	emp_no Integer NOT NULL,
---     dept_no Varchar(4) NOT NULL,
--- 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
--- 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
---     from_date Date NOT NULL,
---     to_date Date NOT NULL
+DROP TABLE IF EXISTS dept_emp;
+CREATE TABLE dept_emp (
+	emp_no Integer NOT NULL,
+    dept_no Varchar(4) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    from_date Date NOT NULL,
+    to_date Date NOT NULL
 	
--- );
+);
 
 -- List the following details of each employee: employee number, last name, first name, gender, and salary.
 
